7b874664479180e752e9a87dbbdd0fb89979f19a
Daniel D. Zhang <idf@users.noreply.github.com>
2015-10-30 17:09:29 -0700

    Create linear_regression.py

---
+++ linear_regression.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class Predictor(object):
+    def linear_regression(self, mat_X, vec_Y):
+        """
+        Learn the parameter \vec{a} for the linear regression model
+        """
+        mat_A = np.dot(mat_X, mat_X.T)
+        vec_b = np.dot(mat_X, vec_Y.T)
+        vec_a = np.dot(np.linalg.inv(mat_A), vec_b)
+        return vec_a
