7e6b8682451099dc44a3d1dca7d38f9ab64f2396
Daniel D. Zhang <idf@users.noreply.github.com>
2016-02-08 23:52:37 -0800

    Delete plotter.py

--- plotter.py
+++
@@ -1,8 +0,0 @@
-import math
-class Plotter(object):
-    def plot(self, L_lst):
-        plt.figure("Log_likelihood")
-        plt.ylabel("L: Log-likelihood")
-        plt.xlabel("N: number of iterations")
-        plt.plot([i+1 for i in xrange(len(L_lst))], L_lst)
-        plt.show()
